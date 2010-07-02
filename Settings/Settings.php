e577c716698a37a46d93fa02a4216ea761483367
Torkild Dyvik Olsen <torkild@tdolsen.net>
2010-07-02 11:08:11 +0200

    layed the basis for Settings\Settings

--- Settings/Settings.php
+++ Settings/Settings.php
@@ -7,11 +7,23 @@ class Settings {
 
     private static function setup() {
         if(!isset(self::$settings)) {
-            self::$settings = array();
+            require 'regain/global_settings.php';
+            self::$settings = $settings;
         }
     }
 
     public static function update(array $settings) {
         self::setup();
+        self::$settings = array_merge(self::$settings, $settings);
+    }
+
+    public static function get($key) {
+        if(isset(self::$settings[$key])) {
+            return self::$settings[$key];
+        }
+    }
+
+    public function __get($key) {
+        return self::get($key);
     }
 }
---
+++ global_settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$settings = array(
+    // Empty for now..
+);
