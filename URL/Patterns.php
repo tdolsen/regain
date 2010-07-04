5b5d0c717b87949302b1696de04c6a84995236e5
Torkild Dyvik Olsen <torkild@tdolsen.net>
2010-07-05 01:38:06 +0200

    added Iterator functionality to URL\Patterns and implemented get_view in URL\Router

--- URL/Patterns.php
+++ URL/Patterns.php
@@ -2,8 +2,9 @@
 
 namespace regain\URL;
 
-class Patterns {
+class Patterns implements \Iterator {
     protected $routes = array();
+    protected $iterator = 0;
 
     public function __construct($patterns) {
         if(is_array($patterns)) {
@@ -19,5 +20,26 @@ class Patterns {
 
     public function add_route($regex, $view) {
         $this->routes[] = array($regex, $view);
+    }
+
+/* Iterator */
+    public function current() {
+        return $this->routes[$this->iterator];
+    }
+
+    public function key() {
+        return $this->iterator;
+    }
+
+    public function next() {
+        $this->iterator++;
+    }
+
+    public function rewind() {
+        $this->iterator = 0;
+    }
+
+    public function valid() {
+        return isset($this->routes[$this->iterator]);
     }
 }
--- URL/Router.php
+++ URL/Router.php
@@ -3,5 +3,19 @@
 namespace regain\URL;
 
 class Router {
+    protected $patterns;
 
+    public function __construct($patterns) {
+        $this->patterns = $patterns;
+    }
+
+    public function get_view($path) {
+        foreach($this->patterns as $pattern) {
+            if($path == $pattern[0]) {
+                return $pattern[1];
+            } elseif(preg_match('#' . preg_quote($pattern[0], '#') . '#', $path)) {
+                return $pattern[1];
+            }
+        }
+    }
 }
