bf37902f07bdd375f0d14eedc94bd712a64d7665
Torkild Dyvik Olsen <torkild@tdolsen.net>
2010-07-05 01:12:07 +0200

    modified constructor for IncludeException to take file as first parameter, and message as second

--- IncludeException.php
+++ IncludeException.php
@@ -2,4 +2,17 @@
 
 namespace regain;
  
-class IncludeException extends Exception {}
+class IncludeException extends Exception {
+    protected $include_file;
+
+    public function __construct($include_file, $text=null) {
+        $this->include_file = $include_file;
+        if(isset($test)) {
+            parent::__construct($text);
+        }
+    }
+
+    public function getIncludeFile() {
+        return $this->include_file;
+    }
+}
