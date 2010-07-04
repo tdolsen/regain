7cb41c6f6626418515a524f811d23e45c512fdda
Torkild Dyvik Olsen <torkild@tdolsen.net>
2010-07-05 01:38:35 +0200

    added basic functionality to HTTP\Request

--- HTTP/Request.php
+++ HTTP/Request.php
@@ -3,5 +3,17 @@
 namespace regain\HTTP;
 
 class Request {
+    protected $data;
 
+    public function __construct() {
+        $data = array();
+
+        $data['path'] = 'tab/';
+
+        $this->data = $data;
+    }
+
+    public function __get($key) {
+        return $this->data[$key];
+    }
 }
