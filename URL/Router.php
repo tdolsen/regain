631c9c6bf6db0586fbd5fc6bb97f1c76e203bcbe
Torkild Dyvik Olsen <torkild@tdolsen.net>
2010-07-05 01:57:59 +0200

    implemented get_view, but still a lot of work

--- URL/Router.php
+++ URL/Router.php
@@ -11,10 +11,17 @@ class Router {
 
     public function get_view($path) {
         foreach($this->patterns as $pattern) {
-            if($path == $pattern[0]) {
-                return $pattern[1];
-            } elseif(preg_match('#' . preg_quote($pattern[0], '#') . '#', $path)) {
-                return $pattern[1];
+            $regex = '#' . str_replace('#', '\#', $pattern[0]) . '#';
+            if(preg_match($regex, $path)) {
+                $ret = $pattern[1];
+
+                if($ret instanceof Patterns) {
+                    // return $ret->get_view($path);
+                    // TODO: Find a way to recursivly fetch the view, while sliceing off the sub path
+                    throw new \regain\NotImplementedException();
+                }
+
+                return $ret;
             }
         }
     }
