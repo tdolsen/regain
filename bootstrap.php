3689882d4ddd93e2a26c08a555db07a8ff5213cb
Torkild Dyvik Olsen <torkild@tdolsen.net>
2010-07-04 21:09:12 +0200

    updated global settings with new default values, and modified bootstrapper heavily to support autoloading and error handling

--- bootstrap.php
+++ bootstrap.php
@@ -1,52 +1,108 @@
 <?php
 
-// Let's make sure files are found
-function __autoload($class) {
-    require_once str_replace(array('\\'), array('/'), $class) . '.php';
-}
-
 use regain\HTTP\Request
+  , regain\HTTP\Response
   , regain\Middleware\Middleware
   , regain\Settings\Settings
   , regain\URL\Router
+  , regain\Exception
+  , regain\IncludeException
   ;
 
-// Fetch the project settings file
-assert('file_exists("settings.php") /* The settings.php file does not exist. Each project need to have at least one when using the bootstrapper. */');
-require 'settings.php';
 
-// Overwrite default settings with project settings
-assert('isset($settings) /* The settings.php file is malformatted. It has to contain an associative array named $settings. */');
-Settings::update($settings);
 
-// Setting up required variables
-$request = new Request();
-$middleware = new Middleware($settings->middleware);
-$settings = new Settings();
-$router = new Router();
+try {
+	/*
+	 TODO: All these system function should be moved to a seperate file and only activated here.
+	*/
 
-// Process request middleware
-$middleware->process_request($request);
+	if(!function_exists('is_includable')) {
+	    function is_includable($file) {
+		$ip = explode(PATH_SEPARATOR, get_include_path());
+		$file = ltrim($file, '/');
 
-// Get the url patterns
-assert('file_exists("' . $settings->urls_file . '.php") /* The urls file "' . $settings->urls_file . '.php" does not exist. Make sure the file exist and that it matches your setting in settings.php. */');
-require $settings->urls_file . '.php';
+		foreach($ip as $path) {
+		    $f = rtrim($path, '/') . '/' . $file;
+		    if(file_exists($f)) {
+			return true;
+		    }
+		}
 
-// TODO: Process urls middleware
-//$middleware->process_urls($request, $patterns);
+		return false;
+	    }
+	}
 
-// Set up the router and get view
-$router = new Router($patterns);
-$view = $router->get_view($request->path);
+	// Throw php errors as ErrorException
+	function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+	    if(strtolower(substr($errstr, 0, 7)) == 'require') {
+		#die("require error");
+	    }
+	    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+	}
+	set_error_handler("exception_error_handler");
 
-// TODO: Process view middleware
-//$middleware->process_view($request, $view);
+	// Let's make sure files are found
+	function __autoload($class) {
+	    $file = str_replace(array('\\'), array('/'), $class) . '.php';
 
-// Run the view
-$response = call_user_func($view, $request);
+	    if(!is_includable($file)) {
+		throw new IncludeException('The file "' . $file . '" is not in include_path, and cannot be included.');
+	    }
 
-// Process response middleware
-$middleware->process_response($request, $response);
+            try {
+                require_once $file;
+            } catch (ErrorException $e) {
+                throw new IncludeException('The file "' . $file . '" is malformatted.');
+            }
+	}
 
-// Get that response out!!!
-echo $response;
+/* END OF SYSTEM FUNCTIONS. ALSO: TODO: Move the to another file, only active here. Yeah. */
+
+	// Fetch the project settings file
+	assert('file_exists("settings.php") /* The settings.php file does not exist. Each project need to have at least one when using the bootstrapper. */');
+	require 'settings.php';
+
+	// Overwrite default settings with project settings
+	assert('isset($settings) /* The settings.php file is malformatted. It has to contain an associative array named $settings. */');
+	$settings = new Settings($settings);
+
+	// Setting up required variables
+	$request = new Request();
+	$middleware = new Middleware($settings->middleware);
+
+	$router = new Router();
+
+	// Process request middleware
+	$middleware->process_request($request);
+
+	// Get the url patterns
+	assert('file_exists("' . $settings->urls_file . '.php") /* The urls file "' . $settings->urls_file . '.php" does not exist. Make sure the file exist and that it matches your setting in settings.php. */');
+	require $settings->urls_file . '.php';
+
+	// TODO: Process urls middleware
+	//$middleware->process_urls($request, $patterns);
+
+	// Set up the router and get view
+	$router = new Router($patterns);
+	$view = $router->get_view($request->path);
+
+	// TODO: Process view middleware
+	//$middleware->process_view($request, $view);
+
+	// Run the view
+	$response = call_user_func($view, $request);
+
+	// And throw an exception if response is of wrong type
+	if(!$response instanceof Response) {
+	throw new UnexpectedValueException('The return value of view functions must be an instance of HTTP\Response');
+	}
+
+	// Process response middleware
+	$middleware->process_response($request, $response);
+
+	// Get that response out!!!
+	echo $response;
+} catch(Exception $e) {
+    echo "<h1>Reached bottom catch</h1>";
+    print_r($e);
+}
--- global_settings.php
+++ global_settings.php
@@ -1,6 +1,15 @@
 <?php
 
 $settings = array(
+    // Debug always off as standard
+    'debug' => false,
+
     // The name of the urls file, excluding .php
-    'urls_file' => 'urls'
+    'urls_file' => 'urls',
+
+    // Standard middleware classes in a simple project
+    'middleware' => array(
+        'regain\Cookies\Middleware\CookiesMiddleware',
+        'regain\Session\Middleware\SessionMiddleware'
+    )
 );
