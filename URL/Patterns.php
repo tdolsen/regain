7a0ccb4e6e5900fd4dd6eac8bdf3f6f97f34f185
Torkild Dyvik Olsen <torkild@tdolsen.net>
2010-07-05 02:07:04 +0200

    moved to get_view to URL\Patterns, and removed then meaningless URL\Router

--- URL/Patterns.php
+++ URL/Patterns.php
@@ -22,6 +22,22 @@ class Patterns implements \Iterator {
         $this->routes[] = array($regex, $view);
     }
 
+    public function get_view($path) {
+        foreach($this->routes as $route) {
+            $regex = '#' . str_replace('#', '\#', $route[0]) . '#';
+            if(preg_match($regex, $path)) {
+                $ret = $route[1];
+
+                if($ret instanceof Patterns) {
+                    $path = preg_replace($regex, '', $path);
+                    return $ret->get_view($path);
+                }
+
+                return $ret;
+            }
+        }
+    }
+
 /* Iterator */
     public function current() {
         return $this->routes[$this->iterator];
