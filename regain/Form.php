bf40cff03191d7df8a22a5a3e61d98d62a36a87d
Torkild Dyvik Olsen <Torkild@.(none)>
2010-07-24 14:39:37 +0200

    forgot to add Form.php

--- regain/Form.php
+++ regain/Form.php
@@ -6,45 +6,72 @@ use regain\Form\FieldInterface
   ;
 
 abstract class Form {
-    protected $fields = array();
-    protected $data;
-    protected $valid;
+    protected $_fields = array();
+    protected $_data;
 
-    public function __construct($data = null, $initial = array()) {
+    public function __construct($data = null, array $initial = array()) {
+        // Figureing out if the form is bound
+        if(!is_null($data) and is_array($data)) {
+            $this->bound = true;
+        } else {
+            $data = array();
+            $this->bound = false;
+        }
+        
         // Setting up the fields
         foreach(get_object_vars($this) as $key => $field) {
             if($field instanceof FieldInterface) {
-                if(isset($initial[$key])) $field->inital = $initial[$key];
-                $this->fields[] = array($key, $field);
+                if(isset($initial[$key])) $field->set_inital($initial[$key]);
+                if(isset($data[$key])) $field->set_value($data[$key]);
+                $this->_fields[] = array($key, $field);
             }
         }
-
-        // Checking for data, and adding to appropriate fields
-        if(!is_null($data) and is_array($data)) {
-            foreach($data as $key => $value) {
-                if(isset($this->fields[$key])) {
-                    $this->fields[$key]->set_value($value);
+    }
+    
+    protected function __set($name, $value) {
+        $this->_data[$name] = $value;
+    }
+    
+    public function __get($name) {
+        if($key == 'errors') {
+            
+        } elseif($key == 'cleaned_data') {
+            
+        } elseif(substr($key, 0, 3) == 'as_') {
+            
+        } else {
+            return $this->_data[$name];
+        }
+    }
+    
+    public function __isset($name) {
+        return isset($this->_data[$name]);
+    }
+    
+    public function __toString() {
+        $out = "";
+        foreach($this->fields as $field) {
+            $out.= (string) $field[1];
+        }
+        return $out;
+    }
+    
+    public function is_bound() {
+        return $this->bound;
+    }
+    
+    public function is_valid() {
+        if(!isset($this->valid)) {
+            $this->valid = true;
+            
+            foreach($this->_fields as $field) {
+                if(!$field[1]->is_valid()) {
+                    $this->valid = false;
+                    break;
                 }
             }
         }
-    }
-
-    public function is_valid() {
-        foreach($this->fields as $field) {
-            if(!$field->is_valid) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public function __get($key) {
-        if($key == 'errors') {
-
-        } elseif($key == 'cleaned_data') {
-
-        } elseif(substr($key, 0, 3) == 'as_') {
-
-        }
+        
+        return $this->valid;
     }
 }
