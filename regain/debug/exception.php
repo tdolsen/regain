6aa4afe38948b6e865c431efd41f23473523ed75
tdolsen <torkild@tdolsen.net>
2010-07-29 01:08:23 +0200

    added some more information for easier debugging

--- regain/debug/exception.php
+++ regain/debug/exception.php
@@ -15,6 +15,10 @@
     
     <div id="body">
         <div>
+            <h2>Exception Class</h2>
+            <p><?php echo get_class($e); ?></p>
+        </div>
+        <div>
             <h2>Message</h2>
             <p><?php echo $e->getMessage(); ?></p>
         </div>
