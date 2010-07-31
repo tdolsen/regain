8a6099ebdd0854012678a40632f7e606a50b92ec
tdolsen <torkild@tdolsen.net>
2010-07-31 04:32:10 +0200

    removed the HTTP\ResponseNotFound class and replaced it with a shortcut function render_not_found, which seems like a more suitable way to handle 404 responses

--- regain/shortcuts.php
+++ regain/shortcuts.php
@@ -8,3 +8,8 @@ function render_to_response($template, $data=null) {
     $t = new Template($template);
     return new Response($t->render($data));
 }
+
+function render_not_found($template = 'errors/404.html') {
+    $t = new Template($template);
+    return new Response($t->render(), 404);
+}
